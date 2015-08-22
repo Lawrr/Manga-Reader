cf2e65a669270d410cb3adf3d4ee49c119c4f3c1
Lawrence Ling <Lawrence1995@hotmail.com>
2015-08-22 16:35:23 +1000

    Added new package for ui/activity

--- app/src/main/java/com/lawrr/mangareader/ui/activity/LauncherActivity.java
+++ app/src/main/java/com/lawrr/mangareader/ui/activity/LauncherActivity.java
@@ -1,9 +1,11 @@
-package com.lawrr.mangareader;
+package com.lawrr.mangareader.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.lawrr.mangareader.R;
 
 public class LauncherActivity extends AppCompatActivity {
 
